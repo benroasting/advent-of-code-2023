1d4627a2fd0695c6759939aa760612475b5de24f
Ben + Steph <developing.ben@gmail.com>
2023-11-26 13:45:16 -0600

    set up for advent of code

---
+++ inputArray.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+function getInput(inputFile, delimiter = "\n") {
+    try {
+        return fs.readFileSync(inputFile, "utf-8").trimEnd().split(delimiter)
+    } catch (err) {
+        console.error(err);
+        process.exit(1)
+    }
+}
+
+module.exports = getInput;
