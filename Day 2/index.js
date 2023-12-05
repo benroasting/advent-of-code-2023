191504820b98f521d10db7ea19f66707e54cbf27
Ben Young <developing.ben@gmail.com>
2023-12-05 12:35:18 -0600

    day 2 solution

--- Day 2/index.js
+++ Day 2/index.js
@@ -1,24 +1,61 @@
-import { options, input } from "../common.mjs";
+import fs from "fs";
 
-const games = input
-  .map((line) => line.split(": ")[1])
-  .map((line) => line.split("; "))
-  .map((set) => set.split(", "))
-  .map((set) => set.map((value) => value.split(" ")))
-  .map((set) =>
-    set.reduce((a, i) => Object.assign(a, { [i[1]]: parseInt(i[0]) }), {})
-  );
-console.log(games);
-
-const check = (games, red, green, blue) => {
-  return games
-    .map((game, i) => {
-      const r = game.filter((set) => set.red > red).length;
-      const g = game.filter((set) => set.green > green).length;
-      const b = game.filter((set) => set.blue > blue).length;
-      return r || g || b ? 0 : i + 1;
-    })
-    .reduce((a, i) => a + i, 0);
+const maxCount = {
+  red: 12,
+  green: 13,
+  blue: 14,
 };
 
-console.log("Part 1", check(games, 12, 13, 14));
+function partOne(file) {
+  const lines = fs.readFileSync(file, "utf-8").trimEnd().split("\n");
+  // Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+  return lines
+    .map((line) => {
+      return line
+        .split(": ")[1]
+        .split("; ")
+        .map((set) => {
+          const pulls = set.split(", ");
+          return pulls.every((pull) => {
+            const [count, color] = pull.split(" ");
+            return count <= maxCount[color];
+          });
+        })
+        .every((play) => play);
+    })
+    .reduce((sum, result, i) => {
+      return result ? sum + (i + 1) : sum;
+    }, 0);
+}
+
+// console.log("Part 1", partOne("./input.txt"));
+
+function partTwo(file) {
+  const lines = fs.readFileSync(file, "utf-8").trimEnd().split("\n");
+
+  return lines
+    .map((line) => {
+      const maxCount = {
+        red: 0,
+        green: 0,
+        blue: 0,
+      };
+      line
+        .split(": ")[1]
+        .split("; ")
+        .forEach((set) => {
+          const pulls = set.split(", ");
+          return pulls.forEach((pull) => {
+            const [count, color] = pull.split(" ");
+            if (maxCount[color] < Number(count)) {
+              maxCount[color] = Number(count);
+            }
+          });
+        });
+      console.log(maxCount);
+      return maxCount.red * maxCount.green * maxCount.blue;
+    })
+    .reduce((sum, result) => sum + result);
+}
+
+console.log("Part 1", partTwo("./input.txt"));
